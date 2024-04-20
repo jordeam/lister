b8d751de85a34432052093960b2a7e0c653c2a33
Roberto B monteiro <jordeam@gmail.com>
2024-04-20 17:46:09 -0300

    Improving menu and showing nbox in component home locations

--- controllers/search.mjs
+++ controllers/search.mjs
@@ -13,21 +13,21 @@ controller.get = asyncHandler(async (req, res, next) => {
 });
 
 controller.post = asyncHandler(async (req, res, next) => {
-  const components = await seqlz.query("SELECT c.id, c.name, g.name AS g_name FROM components AS c, groups AS g WHERE g.id = c.group_id AND c.name ~ $1",
+  const components = await seqlz.query("SELECT c.id, c.name, g.name AS g_name FROM components AS c, groups AS g WHERE g.id = c.group_id AND c.name ~* $1 ORDER BY g.name, c.name",
                                     {
                                       bind: [req.body.expr],
                                       type: QueryTypes.SELECT,
                                     });
 
   const clocs = [];
-  const cloc = await seqlz.query("SELECT l.id,l.name,le.box,le.quant,le.id FROM locations AS l, location_entry AS le, components AS co WHERE l.id = le.location_id AND le.component_id = co.id AND co.name ~ $1 ORDER BY l.name",
+  const cloc = await seqlz.query("SELECT l.id,l.name,le.box,le.quant,le.id FROM locations AS l, location_entry AS le, components AS co WHERE l.id = le.location_id AND le.component_id = co.id AND co.name ~* $1 ORDER BY l.name",
                                  {
                                   bind: [req.body.expr],
                                   type: QueryTypes.SELECT,
                                 });
     clocs.push(cloc);
 
-  const partnumbers = await seqlz.query("SELECT c.id, c.name, g.name AS g_name, sc.manufact_pn, sc.code, s.name AS s_name, m.name AS m_name from suppliercodes AS sc, components AS c, groups AS g, suppliers AS s, manufacturers AS m WHERE manufact_id = m.id AND supplier_id = s.id AND component_id = c.id AND group_id = g.id AND (manufact_pn ~ $1 OR code ~ $1)",
+  const partnumbers = await seqlz.query("SELECT c.id, c.name, g.name AS g_name, sc.manufact_pn, sc.code, s.name AS s_name, m.name AS m_name from suppliercodes AS sc, components AS c, groups AS g, suppliers AS s, manufacturers AS m WHERE manufact_id = m.id AND supplier_id = s.id AND component_id = c.id AND group_id = g.id AND (manufact_pn ~* $1 OR code ~* $1)",
                                     {
                                       bind: [req.body.expr],
                                       type: QueryTypes.SELECT,
